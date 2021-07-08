8084705398aef543f2e95a959c1a16963b0ffc48
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-08 22:33:51 +0300

    add database settings in the seetings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -69,14 +69,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ig',   
+        'USER': 'postgres',      
+        'PASSWORD':'chebichii1',
     }
 }
 
