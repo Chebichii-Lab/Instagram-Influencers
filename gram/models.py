528614c1e3c9d28e6c6919adaf4f94df0048ce50
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 16:29:26 +0300

    add model class image

--- gram/models.py
+++ gram/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from cloudinary.models import CloudinaryField
+
 
 
 # Create your models here.
@@ -8,3 +10,14 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     picture = CloudinaryField('image')
     bio = models.TextField()
+
+
+#class Images
+class Image(models.Model):
+    image = CloudinaryField('images')
+    image_name = models.CharField(max_length=30,blank=True)
+    image_caption = models.CharField(max_length=300)
+    profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    comments = models.CharField(max_length=30,blank=True)
+   
