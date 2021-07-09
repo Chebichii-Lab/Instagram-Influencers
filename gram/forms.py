a5ae07ab5c972d0897e5336d044f44de27869536
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 18:25:05 +0300

    create sign up form

---
+++ gram/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Profile, Image
+from django.contrib.auth.models import User
+
+class SignupForm(forms.Form):
+    emails = forms.EmailField(max_length=300, help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
