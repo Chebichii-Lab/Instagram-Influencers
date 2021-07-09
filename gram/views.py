fd22c3f60a5433c72b7eb995ec04f6e14d02f211
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 14:51:40 +0300

    add navbar

--- gram/views.py
+++ gram/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
 
 
-@login_required(login_url='/accounts/register/')
+@login_required(login_url='/accounts/login/')
 def home(request):
        
     return render(request,"home.html")
