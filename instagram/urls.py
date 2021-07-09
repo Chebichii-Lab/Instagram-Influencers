9dc8030acd7b70d9b1845cc43fb6e02dabb3b5aa
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 12:45:20 +0300

    add logout url

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('gram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.LogoutView.as_view(), {"next_page": '/'}),
 ]
