f610160a0bcff89521ad734b776b8465c0966497
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 17:27:40 +0300

    correct migrations

--- gram/models.py
+++ gram/models.py
@@ -10,7 +10,8 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     picture = CloudinaryField('image')
     bio = models.TextField()
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    
     def __str__(self):
         return self.picture
 
