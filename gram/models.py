bb2d6c996d75fab6bb9946acd97199314b804a98
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 16:34:10 +0300

    add class methods to the image class

--- gram/models.py
+++ gram/models.py
@@ -20,4 +20,19 @@ class Image(models.Model):
     profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes', blank=True, )
     comments = models.CharField(max_length=30,blank=True)
+
+    def __str__(self):
+        return self.image_name
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+
    
