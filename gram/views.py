7ae00da9c9d6bc25d187fa4d3040378c3042268f
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-09 11:58:38 +0300

    add log in decorater

--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+       
+    return render(request,"home.html")
