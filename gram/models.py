09f43ee7448ed15418ff285f78c9a2e77a4b1c63
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-11 11:52:46 +0300

    add Comment class in the models.py

--- gram/models.py
+++ gram/models.py
@@ -53,5 +53,18 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()
 
+# class Comment
+class Comment(models.Model):
+    comment = models.TextField()
+    post= models.ForeignKey(Image, on_delete=models.CASCADE)
+    user= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    def save_comment(self):
+        self.user
+
+    def delete_comment(self):
+        self.delete()
 
    
