3cf79ccafdcd293faff7fff4ef8bfb0f247ac515
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-13 12:18:32 +0300

    add if function in the urls.py

--- gram/urls.py
+++ gram/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 
 urlpatterns=[
@@ -14,3 +15,6 @@ urlpatterns=[
     url('search/', views.search_profile, name='search'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
